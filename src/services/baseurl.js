ba488ec2fd03776a213548d36708d3b16c402dbc
ajay <ajayachu9633@gmail.com>
2023-12-28 14:49:36 +0530

    frontend

--- src/services/baseurl.js
+++ src/services/baseurl.js
@@ -1,3 +1,2 @@
-//export const base_url = 'http://localhost:4000'
+export const base_url = 'https://project-fair-backend1.onrender.com'
 
-export const base_url = 'https://projectfairnew1.onrender.com'
